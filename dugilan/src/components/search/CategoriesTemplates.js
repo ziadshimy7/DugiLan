9c34aa9e7873b8585ecb683e258b98a7a8acab0d
ziadshimy7 <ziadshimy7@gmail.com>
2022-01-10 02:53:36 +0500

    Added sorting feature pt. 1

--- dugilan/src/components/search/CategoriesTemplates.js
+++ dugilan/src/components/search/CategoriesTemplates.js
@@ -9,6 +9,7 @@ const CategoriesTemplates = () => {
   let params = useParams();
   const [templates, setTemplates] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [optionsValue, setOptionsValue] = useState("Ascending");
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 6;
   const { getRequest: getTemplates } = useHTTP(
@@ -26,6 +27,14 @@ const CategoriesTemplates = () => {
 
     setIsLoading(false);
   }, [setIsLoading, getTemplates]);
+  const lastPostIndex = currentPage * itemsPerPage;
+  const firstPostIndex = lastPostIndex - itemsPerPage;
+  const currentPosts = templates?.matches?.slice(firstPostIndex, lastPostIndex);
+  const sortedItems = (sortBy) => {
+    if (sortBy === "Ascending") return currentPosts?.sort();
+    if (sortBy === "Descending") return currentPosts?.sort().reverse();
+    else return currentPosts?.name;
+  };
   useEffect(() => {
     let isSubscribed = true;
     if (isSubscribed) loadTemplates();
@@ -34,12 +43,12 @@ const CategoriesTemplates = () => {
     };
   }, [loadTemplates]);
 
-  const lastPostIndex = currentPage * itemsPerPage;
-  const firstPostIndex = lastPostIndex - itemsPerPage;
-  const currentPosts = templates?.matches?.slice(firstPostIndex, lastPostIndex);
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
+  console.log(optionsValue);
+  const sortedItemsFinal = sortedItems(optionsValue);
+  console.log(sortedItemsFinal);
   return (
     <>
       <Navbar />
@@ -58,12 +67,26 @@ const CategoriesTemplates = () => {
       <div className={styles["dugilan__search"]}>
         <div className={styles["dugilan__search-heading"]}>
           <h1 className="gradient__text">Search results</h1>
+          <div className={styles["dugilan__search-heading_sort"]}>
+            <label htmlFor="sort">Sort Items by</label>
+            <select
+              value={optionsValue}
+              onChange={(e) => {
+                setOptionsValue(e.target.value);
+              }}
+              id="sort"
+            >
+              <option value="Ascending">Ascending</option>
+              <option value="price">Price</option>
+              <option value="Descending">Descending</option>
+            </select>
+          </div>
         </div>
         <Cards
           searchTerm={params.term}
           isLoading={isLoading}
           setIsLoading={setIsLoading}
-          templates={currentPosts}
+          templates={sortedItemsFinal}
         />
         <Pagination
           itemsPerPage={itemsPerPage}
