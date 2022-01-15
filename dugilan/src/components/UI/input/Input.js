5b4c35cddcb4fb4ded15733d4f9c1f28f06a80bc
ziadshimy7 <ziadshimy7@gmail.com>
2022-01-16 03:19:14 +0500

    Added show/hide password in inputs

--- dugilan/src/components/UI/input/Input.js
+++ dugilan/src/components/UI/input/Input.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from "./Input.module.css";
-import { BiShow } from "react-icons/bi";
+import { BiHide, BiShow } from "react-icons/bi";
 
 const Input = (props) => {
   const [showPassword, setShowPassword] = useState(false);
@@ -8,6 +8,18 @@ const Input = (props) => {
     e.preventDefault();
     setShowPassword((prevState) => !prevState);
   };
+  const showHidePasswordToggle = showPassword ? (
+    <BiShow
+      onClick={onShowPasswordHandler}
+      className={styles["dugilan__signIn-icon"]}
+    />
+  ) : (
+    <BiHide
+      onClick={onShowPasswordHandler}
+      className={styles["dugilan__signIn-icon"]}
+    />
+  );
+
   return (
     <>
       <label className={styles["dugilan__signIn-label"]} htmlFor={props.id}>
@@ -26,12 +38,7 @@ const Input = (props) => {
           value={props.value}
           required={true}
         />
-        {props.showPassword && (
-          <BiShow
-            onClick={onShowPasswordHandler}
-            className={styles["dugilan__signIn-icon"]}
-          />
-        )}
+        {props.showPassword && showHidePasswordToggle}
       </div>
     </>
   );
