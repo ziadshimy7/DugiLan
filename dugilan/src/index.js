cd0cfd16f6888f134bd5abac71279af525e49d11
ziadshimy7 <ziadshimy7@gmail.com>
2022-01-18 04:31:41 +0500

    added lazy loading to Cart and Search components

--- dugilan/src/index.js
+++ dugilan/src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Cart from "./components/cart/Cart";
 import { AuthProvider } from "./contexts/AuthContext";
 import ModalProvider from "./contexts/ModalContext";
 import NotFound from "./components/not-found/NotFound";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import CategoriesTemplates from "./components/search/CategoriesTemplates";
 import "./i18next.js";
 import SiteDirectionProvider from "./contexts/SiteDirectionContext";
-
+const Cart = React.lazy(() => import("./components/cart/Cart"));
+const CategoriesTemplates = React.lazy(() =>
+  import("./components/search/CategoriesTemplates")
+);
 ReactDOM.render(
   <Suspense fallback={"."}>
     <React.StrictMode>
